036dda23e113094da7ce6436c5bd1b889127eb1e
Sai Main <saimain.venuslab@gmail.com>
2022-10-13 15:27:23 +0630

    pwa

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,8 @@
 // If you are using 'GenerateSW' (default) for your workboxPluginMode setting this file is auto generated for you.
 // If you are using 'InjectManifest' then add this to your custom SW after your standard workbox config
+import { precacheAndRoute } from "workbox-precaching";
 
+precacheAndRoute(self.__WB_MANIFEST);
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
