0eaf64a19551853a08441c09cc1abcf53cff2ef6
Sai Main <saimain.venuslab@gmail.com>
2022-10-13 15:16:15 +0630

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,32 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 
   pluginOptions: {
     vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+
+  pwa: {
+    name: "BFF",
+    themeColor: "#4c2fe3",
+    msTileColor: "#4c2fe3",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#4c2fe3",
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "src/service-worker.js",
+      // ...other Workbox options...
+    },
+    icons: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/msapplication-icon-144x144.png",
+    },
+  },
+});
