36bd88bcf499a51177ab6eabfd27c5d8bdbdfe4f
justsaimain <saimain.primary@gmail.com>
2022-10-20 16:04:20 +0630

    prediction

--- src/views/Tournament.vue
+++ src/views/Tournament.vue
@@ -138,8 +138,9 @@
 			<v-col>
 				<v-alert
 					v-if="
-						tournamentData.predictions ||
-						tournamentData.predictions.some((p) => p.boosted === true)
+						tournamentData.predications
+							? tournamentData.predictions.some((p) => p.boosted === true)
+							: true
 					"
 					class="mb-3"
 					color="success"
