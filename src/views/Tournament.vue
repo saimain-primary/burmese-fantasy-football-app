672b2a30f4a9d9a9d39f888ed64143faeafc18a8
justsaimain <saimain.primary@gmail.com>
2022-12-14 01:07:13 +0630

    leaderboard

--- src/views/Tournament.vue
+++ src/views/Tournament.vue
@@ -555,7 +555,7 @@ export default {
 					
 
 				this.playerOptions = [...homeTeamPlayers, ...awayTeamPlayers];
-				console.log("🚀 ~ file: Tournament.vue:558 ~ predictionDialogHandler ~ playerOptions", playerOptions)
+				console.log("🚀 ~ file: Tournament.vue:558 ~ predictionDialogHandler ~ playerOptions", this.playerOptions)
 
 				}
 
